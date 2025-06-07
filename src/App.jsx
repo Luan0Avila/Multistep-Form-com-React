65ea15824e6405d582a2504952ac90234457ce9d
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-06 21:47:24 -0300

    criando  hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+//Components
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr';
+import UserForm from './components/UserForm';
+import ReviewForm from './components/ReviewForm';
+import Thanks from './components/Thanks';
+
+
+// Hooks
+import { useForm } from './hooks/UsesForm';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const formComponents = [
+    <UserForm/>,
+    <ReviewForm/>,
+    <Thanks/>
+  ];
+
+  const {currentStep, currentComponent} = useForm(formComponents);
 
   return (
-    <>
-    <h1>React Multistep</h1>
-    </>
+    <div className='app'>
+      <div className='header'>
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container">{currentComponent}</div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious/>
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext/>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
