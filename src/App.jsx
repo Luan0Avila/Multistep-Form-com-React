bf924af367c61b76887d402c54f0ab9738f63f20
Luan0Avila <avila.almeida.luan@gmail.com>
2025-06-06 22:03:19 -0300

    alterando algumas lógicas dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 //Components
 import {GrFormNext, GrFormPrevious} from 'react-icons/gr';
+import {FiSend} from 'react-icons/fi'
 import UserForm from './components/UserForm';
 import ReviewForm from './components/ReviewForm';
 import Thanks from './components/Thanks';
 
 
 // Hooks
-import { useForm } from './hooks/UsesForm';
+import { useForm } from './hooks/UseForm';
 
 import './App.css';
 
@@ -18,7 +19,7 @@ function App() {
     <Thanks/>
   ];
 
-  const {currentStep, currentComponent} = useForm(formComponents);
+  const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = useForm(formComponents);
 
   return (
     <div className='app'>
@@ -28,17 +29,27 @@ function App() {
       </div>
       <div className="form-container">
         <p>etapas</p>
-        <form>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type='button'>
+            {!isFirstStep && (
+              <button type='button'onClick={() => changeStep(currentStep - 1)}>
               <GrFormPrevious/>
               <span>Voltar</span>
             </button>
-            <button type='submit'>
+            )}
+            {!isLastStep ? (
+<button type='submit'>
               <span>Avançar</span>
               <GrFormNext/>
             </button>
+            ): (
+              <button type='button'>
+              <span>Enviar</span>
+              <FiSend/>
+            </button>
+            )}
+            
           </div>
         </form>
       </div>
